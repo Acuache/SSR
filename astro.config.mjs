a71695f5d9b363b914ae64d4e9dcf3c7dc671ec2
AcuacheY <v.acuache15@gmail.com>
2025-11-26 15:39:38 -0500

    Replace Node adapter with Cloudflare adapter in Astro configuration and update dependencies accordingly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import db from "@astrojs/db";
+import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), db()],
   output: "static",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
 });
