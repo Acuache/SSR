092bf419e781707348580c2987bf3e88d0c7c1b5
AcuacheY <v.acuache15@gmail.com>
2025-11-26 15:50:26 -0500

    Update Astro configuration to use static output and Vercel adapter, replacing Cloudflare integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), db()],
-  output: "server",
-  adapter: cloudflare(),
+  output: "static",
+  adapter: vercel(),
 });
